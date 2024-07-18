4951e19f7829ecda6224d301ec6330caa772ab04
Edzer Dionido <jeddionido26@gmail.com>
2024-07-18 11:35:16 +0800

    add new route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\JobController;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [JobController::class, 'index']);
+Route::resource('jobs', JobController::class)->only('index');
