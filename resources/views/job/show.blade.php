8ada16901f40fa2f1cfb58f211d24a18ad6b5d80
Edzer Dionido <jeddionido26@gmail.com>
2024-07-20 10:19:37 +0800

    add components and add other companies job available

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -7,4 +7,13 @@
                 Apply now
             </x-link-button>
        </x-job-card>
+
+       <x-card>
+            <h2 class="text-2xl font-bold">More Jobs from {{ $job->employer->company_name }}</h2>
+            <div class="mb-2">
+                @foreach ($job->employer->job as $job)
+                    <span class="block mt-3 text-slate-500 hover:text-blue-500 hover:font-bold"><a href="{{route('jobs.show', $job)}}">{{$job->title}}</a></span>
+                @endforeach
+            </div>
+       </x-card>
 </x-layout>
