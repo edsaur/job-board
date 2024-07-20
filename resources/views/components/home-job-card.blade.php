a6d866866efdd89821ca016f9b1d003eb76c5a09
Edzer Dionido <jeddionido26@gmail.com>
2024-07-20 10:11:51 +0800

    add company name

--- resources/views/components/home-job-card.blade.php
+++ resources/views/components/home-job-card.blade.php
@@ -1,7 +1,8 @@
 <x-card class="flex flex-col w-full h-full">
     <div class="flex flex-col h-full p-4">
-        <h1 class="text-lg font-bold">{{$job->title}} - {{$job->location}}</h1>
-        <h2 class="text-md text-slate-500">Salary: ${{ number_format($job->salary) }}</h2>
+        <h1 class="text-sm font-bold text-slate-500">{{$job->employer->company_name}}</h1>
+        <h2 class="text-lg font-bold">{{$job->title}} - {{$job->location}}</h2>
+        <h3 class="text-md text-slate-500">Salary: ${{ number_format($job->salary) }}</h3>
 
         <x-tags class="flex-grow mt-2">
             <h3 class="text-sm text-slate-400">Category: {{$job->category}}</h3>
