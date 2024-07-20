ae6e1cac38a5a4ed5fd78668ca547b4f5f2fe486
Edzer Dionido <jeddionido26@gmail.com>
2024-07-20 10:01:18 +0800

    Transfer search logic to a Model Scope

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Job;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class JobController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('job.index', ['jobs' => Job::all(), 'categories' => Job::$categories, 'experiences' => Job::$experience]);
+
+        // VALIDATE THE INCOMING REQUEST 
+            // VALIDATE THE SEARCH BAR
+            // VALIDATE THE MININUM AND MAXIMUM VALUE
+        $this->validate($request, [
+           'search' => 'nullable|string|max:255',
+           'min_val' => 'nullable|integer|min:0',
+           'max_val' => 'nullable|integer|min:0'
+        ]);
+        
+        // FILTER THE JOBS
+        $filters = request()->only(['search', 'min_val', 'max_val', 'experience', 'categories']);
+        $jobs = Job::filter($filters);
+
+        return view('job.index', ['jobs' => $jobs->orderBy('created_at', 'desc')->paginate(10), 'categories' => Job::$categories, 'experiences' => Job::$experience]);
     }
 
     /**
