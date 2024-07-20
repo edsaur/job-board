040b5685015748fe15288f4c2c8215e28b197a59
Edzer Dionido <jeddionido26@gmail.com>
2024-07-20 10:07:14 +0800

    add migration for adding employers table and to add a column in job table

---
+++ database/migrations/2024_07_19_094904_create_employers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Employer;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->foreignIdFor(User::class, 'user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->foreignIdFor(Employer::class, 'employer_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->dropForeignIdFor(Employer::class);
+        });
+        Schema::dropIfExists('employers');
+    }
+};
